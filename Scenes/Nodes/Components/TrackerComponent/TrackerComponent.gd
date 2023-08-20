43ceb84d5f65900a7e4e0429c30d8ff4622aed8f
dantoso <80010223+dantoso@users.noreply.github.com>
2023-08-20 12:10:20 -0300

    feat: tracker component signals

--- Scenes/Nodes/Components/TrackerComponent/TrackerComponent.gd
+++ Scenes/Nodes/Components/TrackerComponent/TrackerComponent.gd
@@ -4,6 +4,9 @@ class_name TrackerComponent
 var foundBody: Node2D
 var lastLocation: Vector2
 
+signal didFind(body: Node2D)
+signal didLose(body: Node2D)
+
 func _init() -> void:
 	body_entered.connect(_onBodyEnter)
 	body_exited.connect(_onBodyExit)
@@ -12,11 +15,13 @@ func _init() -> void:
 func _onBodyEnter(body: Node2D) -> void:
 	lastLocation = body.global_position
 	foundBody = body
+	didFind.emit(body)
 
 
 func _onBodyExit(body: Node2D) -> void:
 	lastLocation = body.global_position
 	foundBody = null
+	didLose.emit(body)
 
 
 func _process(delta: float) -> void:
