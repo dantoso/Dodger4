6c1a284aef3564787bfd5409f42c76258a7ea2a2
dantoso <80010223+dantoso@users.noreply.github.com>
2023-09-04 16:56:16 -0300

    removing print

--- Scenes/Nodes/Components/ShotComponent/Shot.gd
+++ Scenes/Nodes/Components/ShotComponent/Shot.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 func spawn(shotDirection: Vector2) -> void:
 	direction = shotDirection
 	_spawnColliders(shotDirection)
-	print("spawning shot ", direction, colliders)
+
 
 
 func unspawn(_atk: Attack) -> void:
